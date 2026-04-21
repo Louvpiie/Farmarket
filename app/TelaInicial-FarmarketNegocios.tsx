79cfc5981fb1e20f8f3009c52c302e64b87a80b3
Louie Nery <louie.nery1500@gmail.com>
2026-04-21 12:46:30 -0300

    Add files via upload

---
+++ app/TelaInicial-FarmarketNegocios.tsx
@@ -0,0 +1,145 @@
+// TelaInicial.tsx 
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+  StatusBar,
+  SafeAreaView,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  LoginEmail: undefined;
+  CriarConta: undefined;
+  TelaPrincipal: undefined;
+};
+
+type WelcomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList>;
+
+const TelaInicial = () => {
+  const navigation = useNavigation<WelcomeScreenNavigationProp>();
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" translucent backgroundColor="transparent" />
+      <ImageBackground
+        source={require('../assets/images/tela_inicial.png')}
+        style={styles.backgroundImage}
+        resizeMode="cover">
+        <SafeAreaView style={styles.safeArea}>
+          <View style={styles.contentContainer}>
+            <Text style={styles.welcomeText}>SEJA BEM-VINDO(A) À</Text>
+            <Text style={styles.brandName}>Farmarket Negócios</Text>
+
+            <Text style={styles.hintText}>Cadastre sua farmácia e aumente suas vendas online</Text>
+
+            <TouchableOpacity
+              style={styles.buttonPrimary}
+              // onPress={() => navigation.navigate('LoginEmail-FN')}>
+              onPress={() => navigation.navigate('Home-FN')}>
+              <Text style={styles.buttonPrimaryText}>Entrar com e-mail</Text>
+            </TouchableOpacity>
+
+
+            <TouchableOpacity
+              style={styles.buttonTertiary}
+              onPress={() => navigation.navigate('CadastroFarmacia')}>
+              <Text style={styles.buttonTertiaryText}>Quero cadastrar a minha farmácia</Text>
+            </TouchableOpacity>
+
+          </View>
+        </SafeAreaView>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  safeArea: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  contentContainer: {
+    paddingHorizontal: 24,
+    paddingTop: 30,
+    paddingBottom: 40,
+  },
+  welcomeText: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontFamily: 'System',
+    fontWeight: '600',
+    textAlign: 'left',
+    letterSpacing: 1.5,
+    marginBottom: -5,
+  },
+  brandName: {
+    color: '#FFFFFF',
+    fontSize: 36,
+    fontFamily: 'System',
+    fontWeight: '700',
+    textAlign: 'left',
+    marginBottom: 10,
+  },
+  hintText: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: '600',
+    textAlign: 'left',
+    marginBottom: 12,
+  },
+  buttonPrimary: {
+    backgroundColor: '#F0F0F0',
+    borderRadius: 8,
+    paddingVertical: 15,
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  buttonPrimaryText: {
+    color: '#212121',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  buttonSecondary: {
+    backgroundColor: '#000000',
+    borderRadius: 8,
+    paddingVertical: 15,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  buttonSecondaryText: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  buttonTertiary: {
+    borderWidth: 1.5,
+    borderColor: '#FFFFFF',
+    borderRadius: 8,
+    paddingVertical: 15,
+    alignItems: 'center',
+  },
+  buttonAdm: {
+    paddingVertical: 15,
+    alignItems: 'center',
+  },
+  buttonTertiaryText: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: '500',
+  },
+});
+
+export default TelaInicial;
